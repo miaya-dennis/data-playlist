b3006bc23ef57fee0710389c4a6fab33f3364d44
Miaya <myfirefly22@gmail.com>
2026-09-02 21:41:32 -0400

    added scripts

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+
+let songButton = document.getElementById("song-button");
+let artistDisplay1 = document.getElementById("artist-1");
+let trackNameDisplay1 = document.getElementById("track-name-1");
+
+async function loadSongs() {
+	 let response = await fetch()
